8db27d06f6ebf2395cdbd8f7ea173346d9ae467d
njaina <98956159+njaina@users.noreply.github.com>
2022-03-08 15:58:28 +0300

    fix:validation sur w3C validator passé

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+	<link rel="stylesheet" href="./css/principale.css">
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <main>
+		<div class="container px-4 py-5" id="custom-cards">
+			<h1 class="pb-2 border-bottom">Hello, Bonjour, Salama ...</h1>
+
+			<div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+				<div class="col">
+					<div class="card card-cover h-100 overflow-hidden text-white bg-success rounded-5 shadow-lg" >
+						<div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+							<h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Système d'exploitation</h2>
+							<ul class="d-flex list-unstyled mt-auto">
+								<li class="d-flex align-items-center">
+									<small>Harrylepap</small>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="col">
+					<div class="card card-cover h-100 overflow-hidden text-white bg-warning rounded-5 shadow-lg" >
+						<div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+							<h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Connectivité réseaux</h2>
+							<ul class="d-flex list-unstyled mt-auto">
+								<li class="d-flex align-items-center">
+									<small>Harrylepap</small>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="col">
+					<div class="card card-cover h-100 overflow-hidden text-white bg-danger rounded-5 shadow-lg" >
+						<div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+							<h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Gestion de service</h2>
+							<ul class="d-flex list-unstyled mt-auto">
+								<li class="d-flex align-items-center">
+									<small>Harrylepap</small>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
