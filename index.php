9035732d030b071f1edc5db069bdf5b930324044
njaina <hei.njaina.2@gmail.com>
2022-03-08 12:08:49 +0300

    fix:modification des fichier css et index.php pour que ces dernier soient validé

--- index.php
+++ index.php
@@ -1,34 +1,18 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Bootstrap CSS -->
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-	<link href="./css/principale.css" />
-
-
-	<style>
-		.bd-placeholder-img {
-			font-size: 1.125rem;
-			text-anchor: middle;
-			-webkit-user-select: none;
-			-moz-user-select: none;
-			user-select: none;
-		}
-
-		@media (min-width: 768px) {
-			.bd-placeholder-img-lg {
-				font-size: 3.5rem;
-			}
-		}
-	</style>
+	<link rel="stylesheet" href="./css/principale.css">
+	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-
-	<main>
+    <main>
 		<div class="container px-4 py-5" id="custom-cards">
 			<h1 class="pb-2 border-bottom">Hello, Bonjour, Salama ...</h1>
 
@@ -74,13 +58,10 @@
 			</div>
 		</div>
 
-		<div class="b-example-divider"></div>
 
 	</main>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
